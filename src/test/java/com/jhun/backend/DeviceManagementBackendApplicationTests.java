45e502baf00e9cd606ada6d19881acffe49471d4
emo172 <2785703182@qq.com>
2026-03-11 16:56:53 +0800

    chore: 初始化项目仓库

---
+++ src/test/java/com/jhun/backend/DeviceManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhun.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
